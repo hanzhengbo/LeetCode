1e9244378c51ca09868eda7a5d783697a6a8abe7
hanzhengbo <806850270@qq.com>
2018-04-17 15:34:54 +0800

    292. Nim Game

---
+++ C++/292. Nim Game/Nim Game.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 韩正博 on 18/4/17.
+//
+#include "../header.h"
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if (n%4!=0) return true;
+        return false;
+    }
+};
+
