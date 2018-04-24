a4133ccc98b2efea9f515a5805aa69a5ed0cc0f8
hanzhengbo <806850270@qq.com>
2018-04-24 10:43:54 +0800

    540. Single Element in a Sorted Array

--- C++/540. Single Element in a Sorted Array/Single Element in a Sorted Array.cpp
+++ C++/540. Single Element in a Sorted Array/Single Element in a Sorted Array.cpp
@@ -2,26 +2,26 @@
 // Created by 韩正博 on 18/4/24.
 //
 #include "../header.h"
+
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
+    int singleNonDuplicate(vector<int> &nums) {
         int len = nums.size();
         int count = 1;
-        for(int i = 1 ; i < len ; i++){
-            if(nums[i] == nums[i-1]){
+        for (int i = 1; i < len; i++) {
+            if (nums[i] == nums[i - 1]) {
                 count++;
                 continue;
             }
 
-            if(nums[i] != nums[i-1]){
-                if(count == 2){
+            if (nums[i] != nums[i - 1]) {
+                if (count == 2) {
                     count = 1;
-                    if(i == len-1)
-                        return nums[len-1];
+                    if (i == len - 1)
+                        return nums[len - 1];
                     continue;
-                }
-                else
-                    return nums[i-1];
+                } else
+                    return nums[i - 1];
             }
         }
         return 0;
