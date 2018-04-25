bc01cfe967eb8e4b6615d4345159800c583ae6fc
hanzhengbo <806850270@qq.com>
2018-04-25 12:45:02 +0800

    Add the method of dynamic programming

--- C++/121. Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock.cpp
+++ C++/121. Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock.cpp
@@ -6,7 +6,9 @@
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
-        int result = INT8_MIN;
+        /**
+         * 原始做法
+         * int result = INT8_MIN;
         int len = prices.size();
         for (int i = 0; i < len - 1; i++) {
             for (int j = 1 + i; j < len; j++) {
@@ -17,5 +19,20 @@ public:
         if (result < 0)
             result = 0;
         return result;
+         */
+
+        /**
+         * DP做法
+         */
+        int vmax = INT32_MIN;
+        int vmin = INT32_MAX;
+        int len = prices.size();
+        for (int i = 0; i < len; i++) {
+            vmin = min(vmin, prices[i]);
+            vmax = max(vmax, prices[i] - vmin);
+        }
+        if (vmax < 0)
+            vmax = 0;
+        return vmax;
     }
 };
