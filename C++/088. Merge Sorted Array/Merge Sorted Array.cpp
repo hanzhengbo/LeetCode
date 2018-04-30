1f9ad8b93c1833fa71f6b516f4d39ec450d97a36
hanzhengbo <806850270@qq.com>
2018-04-30 23:39:10 +0800

    088. Merge Sorted Array

---
+++ C++/088. Merge Sorted Array/Merge Sorted Array.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 韩正博 on 18/4/29.
+//
+#include "../header.h"
+
+class Solution {
+public:
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n) {
+        for (int i = 1; i <= n; i++) {
+            nums1[m + i - 1] = nums2[i - 1];
+        }
+        sort(nums1.begin(), nums1.end(), less<int>());
+    }
+};
